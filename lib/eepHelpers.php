2f4039b406a16887a1d5e5917896e3672a26b49a
Doug Plant <doug@mugo.ca>
2012-08-09 16:00:02 -0700

    trimming all the values when writing a table

--- lib/eepHelpers.php
+++ lib/eepHelpers.php
@@ -15,10 +15,13 @@ class eep
     {
         // calc column widths
         $widths = array();
-        foreach( $table as $row )
+        foreach( $table as $r => $row )
         {
             foreach( $row as $column => $part )
             {
+                // trim the value
+                $table[ $r ][ $column ] = trim( $part );
+                // and calc the widths
                 if( !isset( $widths[$column] ) )
                 {
                     $widths[ $column ] = strlen( $part );
